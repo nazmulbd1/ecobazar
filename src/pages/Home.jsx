edda2c18e21d48d740edff59150809ea106f5525
nazmulbd1 <nazmulsheikhnahid@gmail.com>
2026-05-12 14:29:50 +0600

    Update 56 class

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Navbar from '../components/Navbar'
+import React from "react";
+import { useSelector } from "react-redux";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router";
+import Container from "../components/layouts/Container";
 
 const Home = () => {
-    // let data = useSelector((state)=> console.log(state));
+  // let data = useSelector((state)=> console.log(state));
 
   return (
     <>
-        <div>Home</div>
+      <Container>
+        <Link
+          to="/login"
+          className="text-[#1A1A1A] cursor-pointer hover:text-primary"
+        >
+          Login
+        </Link>
+        <br />
+        <Link
+          to="/registration"
+          className="text-[#1A1A1A] cursor-pointer hover:text-primary"
+        >
+          Register
+        </Link>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
