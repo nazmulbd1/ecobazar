6ae3f547d7bb8ec3c3aacc0eaea882fdbc9c3f52
nazmulbd1 <nazmulsheikhnahid@gmail.com>
2026-06-09 12:12:44 +0600

    Update

--- src/components/ProductShowcase.jsx
+++ src/components/ProductShowcase.jsx
@@ -1,9 +1,28 @@
 import React from "react";
 import Container from "./layouts/Container";
-import Product1 from "../assets/images/Product1.webp"
+import Product1 from "../assets/images/Product1.webp";
 import { Link } from "react-router";
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
 
 const ProductShowcase = ({ allData }) => {
+
+  function getStar(count) {
+    let halfStar = count.toString().split('.')[1]
+    let index = Math.floor(count)
+    let arr = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= count) {
+        arr.push("color");
+      } else {
+        arr.push(i);
+      }
+    }
+    if (halfStar){
+      arr[index] = "half"
+    }
+    
+    return arr;
+  }
 
   return (
     <div>
@@ -16,6 +35,21 @@ const ProductShowcase = ({ allData }) => {
               <img src={Product1} alt="Product1" />
               <h3>{item.name || item.title}</h3>
               <p>{item.price && item.price}</p>
+
+              <p>{item.rating && item.rating}</p>
+              <div className="flex">
+                {item.rating &&
+                  getStar(item.rating).map((item) =>
+                    item == "color" ? (
+                      <FaStar className="text-yellow-300" />
+                    ) : (
+                      item == "half" ?
+                      <FaStarHalfAlt className="text-yellow-300"/>
+                      :
+                      <FaStar />
+                    ),
+                  )}
+              </div>
             </div>
           ))}
         </div>
