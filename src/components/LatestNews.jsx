18f7943054d21fcfd924d770a2117a9a5719824f
nazmulbd1 <nazmulsheikhnahid@gmail.com>
2026-06-24 01:56:54 +0600

    update

--- src/components/LatestNews.jsx
+++ src/components/LatestNews.jsx
@@ -1,77 +1,117 @@
-import React from 'react'
-import Container from './layouts/Container'
-import { MoveRight, PencilIcon } from 'lucide-react'
-import Latestnews1 from "../assets/images/Latestnews1.webp"
-import { RiAdminFill } from 'react-icons/ri'
-import { BiComment } from 'react-icons/bi'
-import { Link } from 'react-router'
+import React from "react";
+import Container from "./layouts/Container";
+import { MoveRight, PencilIcon } from "lucide-react";
+import Latestnews1 from "../assets/images/Latestnews1.webp";
+import { RiAdminFill } from "react-icons/ri";
+import { BiComment } from "react-icons/bi";
+import { Link } from "react-router";
 
 const LatestNews = () => {
   return (
     <div>
-        <Container>
-            <div className='font-pop'>
-                <h3>Latest News</h3>
-                <div className='flex'>
-                    <div>
-                        <img src={Latestnews1} alt="Latestnews1" />
-                        <div className='flex gap-6'>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <PencilIcon/> Food
-                            </div>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <RiAdminFill/> By Adim
-                            </div>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <BiComment/> 65 Comments
-                            </div>
-                        </div>
-                        <div>
-                            <p className='text-[18px] text-[#2C742F] font-pop font-medium'>Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.</p>
-                            <Link to="#" className='flex gap-2 text-[16px] text-[#00B207]'>Read More <MoveRight/> </Link>
-                        </div>
+      <Container>
+        <div className="font-pop pb-15">
+          <h3 className="text-[32px] text-[#1A1A1A] font-semibold font-pop text-center pb-8">
+            Latest News
+          </h3>
+          <div className="flex gap-6">
+            <div className="rounded-lg shadow-[0_0_10px_0px_rgba(0,0,0,0.1)] overflow-hidden">
+              <div className="transform-gpu transition-transform duration-300 ease-in-out will-change-transform hover:scale-[1.03]">
+                <img src={Latestnews1} alt="Latestnews1" />
+                <div className="px-6 py-6">
+                  <div className="flex gap-6">
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <PencilIcon /> Food
                     </div>
-                    <div>
-                        <img src={Latestnews1} alt="Latestnews1" />
-                        <div className='flex gap-6'>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <PencilIcon/> Food
-                            </div>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <RiAdminFill/> By Adim
-                            </div>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <BiComment/> 65 Comments
-                            </div>
-                        </div>
-                        <div>
-                            <p className='text-[18px] text-[#2C742F] font-pop font-medium'>Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.</p>
-                            <Link to="#" className='flex gap-2 text-[16px] text-[#00B207]'>Read More <MoveRight/> </Link>
-                        </div>
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <RiAdminFill /> By Adim
                     </div>
-                    <div>
-                        <img src={Latestnews1} alt="Latestnews1" />
-                        <div className='flex gap-6'>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <PencilIcon/> Food
-                            </div>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <RiAdminFill/> By Adim
-                            </div>
-                            <div className='flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5'> 
-                                <BiComment/> 65 Comments
-                            </div>
-                        </div>
-                        <div>
-                            <p className='text-[18px] text-[#2C742F] font-pop font-medium'>Curabitur porttitor orci eget neque accumsan venenatis. Nunc fermentum.</p>
-                            <Link to="#" className='flex gap-2 text-[16px] text-[#00B207]'>Read More <MoveRight/> </Link>
-                        </div>
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <BiComment /> 65 Comments
                     </div>
+                  </div>
+                  <div>
+                    <p className="text-[18px] text-[#2C742F] font-pop font-medium pt-2 pb-5">
+                      Curabitur porttitor orci eget neque accumsan venenatis.
+                      Nunc fermentum.
+                    </p>
+                    <Link
+                      to="#"
+                      className="flex gap-2 text-[16px] text-[#00B207]"
+                    >
+                      Read More <MoveRight />{" "}
+                    </Link>
+                  </div>
                 </div>
+              </div>
             </div>
-        </Container>
-    </div>
-  )
-}
 
-export default LatestNews
+            <div className="rounded-lg shadow-[0_0_10px_0px_rgba(0,0,0,0.1)] overflow-hidden">
+              <div className="transform-gpu transition-transform duration-300 ease-in-out will-change-transform hover:scale-[1.03]">
+                <img src={Latestnews1} alt="Latestnews1" />
+                <div className="px-6 py-6">
+                  <div className="flex gap-6">
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <PencilIcon /> Food
+                    </div>
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <RiAdminFill /> By Adim
+                    </div>
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <BiComment /> 65 Comments
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-[18px] text-[#2C742F] font-pop font-medium pt-2 pb-5">
+                      Curabitur porttitor orci eget neque accumsan venenatis.
+                      Nunc fermentum.
+                    </p>
+                    <Link
+                      to="#"
+                      className="flex gap-2 text-[16px] text-[#00B207]"
+                    >
+                      Read More <MoveRight />{" "}
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="rounded-lg shadow-[0_0_10px_0px_rgba(0,0,0,0.1)] overflow-hidden">
+              <div className="transform-gpu transition-transform duration-300 ease-in-out will-change-transform hover:scale-[1.03]">
+                <img src={Latestnews1} alt="Latestnews1" />
+                <div className="px-6 py-6">
+                  <div className="flex gap-6">
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <PencilIcon /> Food
+                    </div>
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <RiAdminFill /> By Adim
+                    </div>
+                    <div className="flex items-center text-[16px] text-[#4D4D4D] font-pop gap-1.5">
+                      <BiComment /> 65 Comments
+                    </div>
+                  </div>
+                  <div>
+                    <p className="text-[18px] text-[#2C742F] font-pop font-medium pt-2 pb-5">
+                      Curabitur porttitor orci eget neque accumsan venenatis.
+                      Nunc fermentum.
+                    </p>
+                    <Link
+                      to="#"
+                      className="flex gap-2 text-[16px] text-[#00B207]"
+                    >
+                      Read More <MoveRight />{" "}
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default LatestNews;
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,31 +1,93 @@
-import React from 'react'
-import Container from './layouts/Container'
-
+import React from "react";
+import Container from "./layouts/Container";
+import TestiCommaIcon from "../icons/TestiCommaIcon";
+import Client1 from "../assets/images/Client1.webp";
 
 const Testimonial = () => {
   return (
-    <Container>
+    <div className="bg-gray-100 pb-15">
+      <Container>
         <div>
-            <div>
-                <h3 className='text-[32px] font-pop font-bold py-6'>Client Testimonials</h3>
+          <div>
+            <h3 className="text-[32px] font-pop font-bold py-6">
+              Client Testimonials
+            </h3>
+          </div>
+          <div className="flex gap-6">
+            <div className="bg-white px-6 py-6 rounded-lg">
+              <TestiCommaIcon />
+              <p className="text-[14px] text-[#4D4D4D] font-pop pt-4 pb-6">
+                Pellentesque eu nibh eget mauris congue mattis mattis nec
+                tellus. Phasellus imperdiet elit eu magna dictum, bibendum
+                cursus velit sodales. Donec sed neque eget
+              </p>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <img src={Client1} alt="Client1" />
+                  <div>
+                    <h4 className="text-[16px] text-[#1A1A1A] font-pop font-medium">
+                      Robert Fox
+                    </h4>
+                    <p className="text-[14px] text-[#999999] font-pop">Customer</p>
+                  </div>
+                </div>
+                <div>Star</div>
+              </div>
             </div>
-        </div>
-      {/* <div className='my-15 '>
-        <h3 className='font-pop font-semibold text-[32px] text-[#1A1A1A] mb-8'>Client Testimonials</h3>
-        <div className='flex gap-6 '>
-          <div className='max-w-[424px]'>
-            <img src={TestimonialOne} alt="Testimonial One" />
-          </div>
-          <div className='max-w-[424px]' >
-            <img src={TestimonialTwo} alt="Testimonial Two" />
-          </div>
-          <div className='max-w-[424px]'>
-            <img src={TestimonialThree} alt="Testimonial Three" />
-          </div>
-        </div>
-      </div> */}
-    </Container>
-  )
-}
 
-export default Testimonial
+            <div className="bg-white px-6 py-6 rounded-lg">
+              <TestiCommaIcon />
+              <p className="text-[14px] text-[#4D4D4D] font-pop pt-4 pb-6 text-justify">
+                Pellentesque eu nibh eget mauris congue mattis mattis nec
+                tellus. Phasellus imperdiet elit eu magna dictum, bibendum
+                cursus velit sodales. Donec sed neque eget
+              </p>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <img src={Client1} alt="Client1" />
+                  <div>
+                    <h4 className="text-[16px] text-[#1A1A1A] font-pop font-medium">
+                      Robert Fox
+                    </h4>
+                    <p className="text-[14px] text-[#999999] font-pop">Customer</p>
+                  </div>
+                </div>
+                <div>Star</div>
+              </div>
+            </div>
+
+            <div className="bg-white px-6 py-6 rounded-lg">
+              <TestiCommaIcon />
+              <p className="text-[14px] text-[#4D4D4D] font-pop pt-4 pb-6">
+                Pellentesque eu nibh eget mauris congue mattis mattis nec
+                tellus. Phasellus imperdiet elit eu magna dictum, bibendum
+                cursus velit sodales. Donec sed neque eget
+              </p>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-3">
+                  <img src={Client1} alt="Client1" />
+                  <div>
+                    <h4 className="text-[16px] text-[#1A1A1A] font-pop font-medium">
+                      Robert Fox
+                    </h4>
+                    <p className="text-[14px] text-[#999999] font-pop">Customer</p>
+                  </div>
+                </div>
+                <div>Star</div>
+              </div>
+            </div>
+
+
+
+
+
+
+
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Testimonial;
