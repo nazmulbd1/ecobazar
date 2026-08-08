bae223b9a177382ad3ce54f81cc3c74868caaa3d
nazmulbd1 <nazmulsheikhnahid@gmail.com>
2026-08-08 23:37:07 +0600

    update

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -84,39 +84,39 @@ const Shop = () => {
           >
             <div className="flex py-2 pt-5">
               <input id="rating1" type="checkbox" />
-              <label className="flex gap-1 text-[14px] text-[#FF8A00] items-center pl-3" htmlFor="rating1">
+              <label className="flex gap-1 text-[#FF8A00] items-center pl-3" htmlFor="rating1">
                 <FaStar/><FaStar/><FaStar/><FaStar/><FaStar/> 
                 <span className="text-[14px] text-[#1A1A1A] pl-2">5.0</span>
               </label>
             </div>
             <div className="flex py-2">
               <input id="rating2" type="checkbox" />
-              <label className="flex gap-1 text-[14px] text-[#FF8A00] items-center pl-3" htmlFor="rating2">
-                <FaStar/><FaStar/><FaStar/><FaStar/>
+              <label className="flex gap-1 text-[#FF8A00] items-center pl-3" htmlFor="rating2">
+                <span className="flex gap-1"><FaStar/><FaStar/><FaStar/><FaStar/></span> 
                 <span className="text-[#CCCCCC]"><FaStar/> </span> 
                 <span className="text-[14px] text-[#1A1A1A] pl-2">4.0 & up</span>
               </label>
             </div>
             <div className="flex py-2">
               <input id="rating3" type="checkbox" />
-              <label className="flex gap-1 text-[14px] text-[#FF8A00] items-center pl-3" htmlFor="rating3">
-                <FaStar/><FaStar/><FaStar/>
+              <label className="flex gap-1 text-[#FF8A00] items-center pl-3" htmlFor="rating3">
+                <span className="flex gap-1"><FaStar/><FaStar/><FaStar/></span> 
                 <span className="flex gap-1 text-[#CCCCCC]"><FaStar/><FaStar/></span> 
                 <span className="text-[14px] text-[#1A1A1A] pl-2">3.0 & up</span>
               </label>
             </div>
             <div className="flex py-2">
               <input id="rating4" type="checkbox" />
-              <label className="flex gap-1 text-[14px] text-[#FF8A00] items-center pl-3" htmlFor="rating4">
-                <FaStar/><FaStar/>
+              <label className="flex gap-1 text-[#FF8A00] items-center pl-3" htmlFor="rating4">
+                <span className="flex gap-1"><FaStar/><FaStar/></span> 
                 <span className="flex gap-1 text-[#CCCCCC]"><FaStar/><FaStar/><FaStar/> </span> 
                 <span className=" text-[14px] text-[#1A1A1A] pl-2">2.0 & up</span>
               </label>
             </div>
             <div className="flex py-2">
               <input id="rating5" type="checkbox" />
-              <label className="flex gap-1 text-[14px] text-[#FF8A00] items-center pl-3" htmlFor="rating5">
-                <FaStar/>
+              <label className="flex gap-1 text-[#FF8A00] items-center pl-3" htmlFor="rating5">
+                <span className="flex gap-1"><FaStar/></span> 
                 <span className="flex gap-1 text-[#CCCCCC]"><FaStar/><FaStar/><FaStar/><FaStar/> </span> 
                 <span className="text-[14px] text-[#1A1A1A] pl-2">1.0 & up</span>
               </label>
